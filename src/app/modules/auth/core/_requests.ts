0f70d04850cea8d6a0a498829387619ba56cd9b8
dev <devmaxup@gmail.com>
2024-03-19 12:27:59 +0200

    Added Facebook ads page

--- src/app/modules/auth/core/_requests.ts
+++ src/app/modules/auth/core/_requests.ts
@@ -9,6 +9,7 @@ export const GET_USER_BY_ID = `${API_URL}/profiles`;
 export const REGISTER_URL = `${API_URL}/register`;
 export const REQUEST_PASSWORD_URL = `${API_URL}/forgot-password`;
 export const RESET_PASSWORD_URL = `${API_URL}/reset-password`;
+export const GET_FACEBOOK_ACCOUNTS = `${API_URL}/facebook/accounts/facebook`;
 
 // Server should return AuthModel
 export function login(email: string, password: string) {
@@ -18,13 +19,21 @@ export function login(email: string, password: string) {
   });
 }
 
+export function getFacebookAccounts(jwtToken: string) {
+  return axios.get(GET_FACEBOOK_ACCOUNTS, {
+    headers: {
+      Authorization: `Bearer ${jwtToken}`,
+    },
+  });
+}
+
 // Server should return AuthModel
 export function register(
   email: string,
   firstName: string,
   lastName: string,
   password: string,
-  companyName: string,
+  companyName: string
 ) {
   return axios.post(REGISTER_URL, {
     email,
@@ -43,12 +52,20 @@ export function getUserById(userId: string, jwtToken: string) {
   });
 }
 
-export function updateUserById(userId: string, jwtToken: string, userToUpdate: UserByIdProps) {
-  return axios.patch<UserByIdProps>(`${GET_USER_BY_ID}/${userId}`, userToUpdate, {
-    headers: {
-      Authorization: `Bearer ${jwtToken}`,
-    },
-  });
+export function updateUserById(
+  userId: string,
+  jwtToken: string,
+  userToUpdate: UserByIdProps
+) {
+  return axios.patch<UserByIdProps>(
+    `${GET_USER_BY_ID}/${userId}`,
+    userToUpdate,
+    {
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+    }
+  );
 }
 
 // Server should return object => { result: boolean } (Is Email in DB)
---
+++ src/app/pages/facebook-ads/FacebookAds.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const FacebookAds: React.FC = () => {
+  return <div>FacebookAds</div>;
+};
+
+export default FacebookAds;
